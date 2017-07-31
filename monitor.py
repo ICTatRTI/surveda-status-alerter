961009389c9c304acf1996a698b4279e3f17f371
Adam Preston <adam.m.preston@gmail.com>
2017-07-31 15:47:55 -0600

    fixed a type problem

--- monitor.py
+++ monitor.py
@@ -6,6 +6,11 @@ import os
 import pandas as pd
 import io
 from sparkpost import SparkPost
+from os.path import join, dirname
+from dotenv import load_dotenv
+
+dotenv_path = join(dirname(__file__), '.env')
+load_dotenv(dotenv_path)
 
 
 parser = argparse.ArgumentParser()
@@ -17,8 +22,10 @@ if len(sys.argv) < 2:
     parser.print_help()
     sys.exit(1)
 
-
 SURVEDA_URL = os.environ.get("SURVEDA_URL")
+EMAIL_LIST = str(os.environ.get("SURVEDA_EMAIL_LIST")).split()
+SURVEDA_PROJECT = str(args.project)
+SURVEDA_SURVEY = str(args.survey)
 
 # Get the login token
 page = requests.get(SURVEDA_URL+'/sessions/new')
@@ -56,15 +63,16 @@ s.headers.update(headers)
 
 
 # Get Survey Name
-survey = s.get(SURVEDA_URL+'/api/v1/projects/'+args.project+'/surveys/'+args.survey)
+survey = s.get(SURVEDA_URL+'/api/v1/projects/'+SURVEDA_PROJECT+'/surveys/'+SURVEDA_SURVEY)
 survey_name = survey.json()['data']['name']
 
 # Get Interactions
-r = s.get(SURVEDA_URL+'/api/v1/projects/'+args.project+'/surveys/'+args.survey+'/respondents/interactions_csv')
+r = s.get(SURVEDA_URL+'/api/v1/projects/'+SURVEDA_PROJECT+'/surveys/'+SURVEDA_SURVEY+'/respondents/interactions_csv')
 interactions=pd.read_csv(io.StringIO(r.content.decode('utf-8')))
 
-
 # Prepare data for notification
+interactions['Timestamp'] = pd.to_datetime(interactions['Timestamp']);
+interactions = interactions.set_index('Timestamp')
 timegrouped = interactions.groupby([pd.TimeGrouper('1D'), 'Channel'])
 dataframe = timegrouped['Respondent ID'].count().to_frame()
 dataframe = dataframe.reset_index()
@@ -72,7 +80,7 @@ dataframe = dataframe.reset_index()
 channel_data = []
 for index, row in dataframe.iterrows():
     channel = {}
-    channel['date'] = row["Timestamp"]
+    channel['date'] = row["Timestamp"].strftime("%Y-%m-%d")
     channel['name'] = row["Channel"]
     channel['count'] = row["Respondent ID"]
     channel_data.append(channel)
